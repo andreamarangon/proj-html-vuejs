6a42e198bf2712454a04f11dde49fefeddd35a6a
Andrea Marangon <marangonandrea84@gmail.com>
2021-04-23 17:28:13 +0200

    sezione parners terminata con aggiunta di v-for per immagini

--- dist/js/main.js
+++ dist/js/main.js
@@ -29,6 +29,25 @@ var app = new Vue({
     }, {
       name: 'contact',
       link: 'https://demo.phlox.pro/business-2/contact-page/'
+    }],
+    partnersLogo: [{
+      name: 'avantgarde',
+      img: './dist/img/logo-avantgarde.png'
+    }, {
+      name: 'fastlane',
+      img: './dist/img/logo-fastlane.png'
+    }, {
+      name: 'rj',
+      img: './dist/img/logo-rj.png'
+    }, {
+      name: 'chippy',
+      img: './dist/img/logo-chippy.png'
+    }, {
+      name: 'bullseye',
+      img: './dist/img/logo-bullseye.png'
+    }, {
+      name: 'mighty',
+      img: './dist/img/logo-mighty.png'
     }]
   },
   computed: {},
